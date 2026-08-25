2adf7d3022d3b2cc06e644390f783682796e9ee6
Captain-Nimosi <nimosipaik@gmail.com>
2026-08-25 14:47:56 +0900

    Enhance search functionality to maintain active state
    Added functionality to keep the search container visible after executing a search.

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,3 +1,4 @@
+const searchContainer = document.querySelector(".search-container");
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
 const searchResults = document.getElementById("search-results");
@@ -13,11 +14,13 @@ fetch("/nimosi/search.json")
     console.error("Search data could not be loaded:", error);
   });
 
+
 function performSearch() {
   const query = searchInput.value.trim().toLowerCase();
 
   if (!query) {
     searchResults.innerHTML = "";
+    searchContainer.classList.remove("search-active");
     return;
   }
 
@@ -28,28 +31,48 @@ function performSearch() {
     )
     .sort((a, b) => new Date(b.date) - new Date(a.date));
 
+  /*
+   * 검색이 실행되면
+   * hover하지 않아도 검색창을 계속 표시
+   */
+  searchContainer.classList.add("search-active");
+
   if (results.length === 0) {
     searchResults.innerHTML = "<p>No results.</p>";
     return;
   }
 
   searchResults.innerHTML = results.map(post => `
-  <article class="search-result">
-    <div>
-      <a href="${post.url}">${post.date} ${post.title}</a>
-    </div>
-    <div>
-      ${post.category}
-    </div>
-  </article>
-  `).join("");
+    <article class="search-result">
 
+      <div>
+        <a href="${post.url}">
+          ${post.date} ${post.title}
+        </a>
+      </div>
+
+      <div>
+        ${post.category}
+      </div>
+
+    </article>
+  `).join("");
+}
+
+
+/*
+ * Enter로 검색
+ */
 searchInput.addEventListener("keydown", function(event) {
   if (event.key === "Enter") {
     performSearch();
   }
 });
 
+
+/*
+ * go 버튼으로 검색
+ */
 searchButton.addEventListener("click", function() {
   performSearch();
 });
